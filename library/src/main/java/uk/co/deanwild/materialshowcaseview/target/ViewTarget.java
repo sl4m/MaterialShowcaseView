b6374a5329fdf2543426ff26bdc4d8e7f438def0
skim <skim.la@gmail.com>
2018-02-12 14:57:14 -0800

    optionally constrain target bounds if target is too big

--- library/src/main/java/uk/co/deanwild/materialshowcaseview/target/ViewTarget.java
+++ library/src/main/java/uk/co/deanwild/materialshowcaseview/target/ViewTarget.java
@@ -9,9 +9,15 @@ import android.view.View;
 public class ViewTarget implements Target {
 
     private final View mView;
+    private double boundsContraint;
 
     public ViewTarget(View view) {
-        mView = view;
+        this(view, 1);
+    }
+
+    public ViewTarget(View view, double boundsContraint) {
+        this.mView = view;
+        this.boundsContraint = normalizeBounds(boundsContraint);
     }
 
     public ViewTarget(int viewId, Activity activity) {
@@ -20,22 +26,35 @@ public class ViewTarget implements Target {
 
     @Override
     public Point getPoint() {
-        int[] location = new int[2];
-        mView.getLocationInWindow(location);
-        int x = location[0] + mView.getWidth() / 2;
-        int y = location[1] + mView.getHeight() / 2;
+        int[] location = getLocation();
+        int x, y;
+        x = location[0] + mView.getWidth() / 2;
+        y = location[1] + mView.getHeight() / 2;
         return new Point(x, y);
     }
 
     @Override
     public Rect getBounds() {
+        int[] location = getLocation();
+        int right, bottom;
+        right = (int)(location[0] + mView.getMeasuredWidth() * boundsContraint);
+        bottom = (int)(location[1] + mView.getMeasuredHeight() * boundsContraint);
+        return new Rect(location[0], location[1], right, bottom);
+    }
+
+    private int[] getLocation() {
         int[] location = new int[2];
         mView.getLocationInWindow(location);
-        return new Rect(
-                location[0],
-                location[1],
-                location[0] + mView.getMeasuredWidth(),
-                location[1] + mView.getMeasuredHeight()
-        );
+        return location;
+    }
+
+    private double normalizeBounds(double boundsContraint) {
+        if (boundsContraint > 1) {
+            return 1;
+        } else if (boundsContraint < 0.1) {
+            return 0.1;
+        } else {
+            return boundsContraint;
+        }
     }
 }
